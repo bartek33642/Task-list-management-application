361130e79cfd5ab7f5b2923babe1db5740c2628b
bartek33642 <92989898+bartek33642@users.noreply.github.com>
2023-06-11 13:36:00 +0200

    add new directive sorting list

--- task-list-management/src/app/components/task-list/task-list.component.ts
+++ task-list-management/src/app/components/task-list/task-list.component.ts
@@ -1,4 +1,56 @@
-import { Component, OnInit, Input, importProvidersFrom } from '@angular/core';
+// import { Component, OnInit, Input, importProvidersFrom } from '@angular/core';
+// import { DataService } from 'src/app/services/data.service';
+// import { ActivatedRoute } from '@angular/router';
+
+// @Component({
+//   selector: 'task-list',
+//   templateUrl: './task-list.component.html',
+//   styleUrls: ['./task-list.component.css']
+// })
+// export class TaskListComponent implements OnInit {
+
+//   public items$: any;
+//   // @Input() id?: string;
+//   // @Input() title?: string;
+//   // @Input() text?: string;
+//   // @Input() important?: boolean;
+//   // @Input() date_end?: Date;
+
+
+//   constructor(private service: DataService){
+
+//   }
+// ngOnInit() {
+//   this.getAll();
+// }
+
+// // getAll(){
+// //   this.service.getAll().subscribe(response => {
+   
+// //     this.items$ = response; 
+// //   });
+// // }
+// getAll() {
+//   this.service.getAll().subscribe(
+//     response => {
+//       this.items$ = response;
+//       console.log(this.items$);
+//     },
+//     error => {
+//       console.log('Błąd podczas pobierania danych:', error);
+//     }
+//   );
+// }
+
+// deleteTask(id: string) {
+//   this.service.deleteTask(id).subscribe(() => {
+//     this.getAll();
+//   });
+// }
+
+// }
+
+import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -8,44 +60,47 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit {
+  public items$: any[] = [];
 
-  public items$: any;
-  // @Input() id?: string;
-  // @Input() title?: string;
-  // @Input() text?: string;
-  // @Input() important?: boolean;
-  // @Input() date_end?: Date;
+  constructor(private service: DataService) {}
 
-
-  constructor(private service: DataService){
-
-  }
-ngOnInit() {
-  this.getAll();
-}
-
-// getAll(){
-//   this.service.getAll().subscribe(response => {
-   
-//     this.items$ = response; 
-//   });
-// }
-getAll() {
-  this.service.getAll().subscribe(
-    response => {
-      this.items$ = response;
-      console.log(this.items$);
-    },
-    error => {
-      console.log('Błąd podczas pobierania danych:', error);
-    }
-  );
-}
-
-deleteTask(id: string) {
-  this.service.deleteTask(id).subscribe(() => {
+  ngOnInit() {
     this.getAll();
-  });
-}
+  }
 
+  getAll() {
+    this.service.getAll().subscribe(
+    (response: Object) => {
+        this.items$ = response as any[];
+        
+        this.sortTasks();
+      },
+      (error: any) => {
+        console.log('Błąd podczas pobierania danych:', error);
+      }
+    );
+  }
+
+  sortTasks() {
+    this.items$.sort((a, b) => {
+      // Sortowanie po atrybucie 'important' (true na górze)
+      if (a.important && !b.important) {
+        return -1;
+      } else if (!a.important && b.important) {
+        return 1;
+      }
+
+      // Sortowanie po atrybucie 'date_end' (od najwcześniejszej do najpóźniejszej)
+      const dateA = new Date(a.date_end);
+      const dateB = new Date(b.date_end);
+
+      return dateA.getTime() - dateB.getTime();
+    });
+  }
+
+  deleteTask(id: string) {
+    this.service.deleteTask(id).subscribe(() => {
+      this.getAll();
+    });
+  }
 }
--- task-list-management/src/app/directives/background-important.directive.ts
+++ task-list-management/src/app/directives/background-important.directive.ts
@@ -1,18 +1,45 @@
-import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
+// import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
+
+// @Directive({
+//   selector: '[appBackgroundImportant]'
+// })
+// export class BackgroundImportantDirective implements OnChanges {
+//   @Input('appBackgroundImportant') important!: boolean;
+
+//   constructor(private elementRef: ElementRef) {}
+
+//   ngOnChanges() {
+//     if (this.important) {
+//       this.elementRef.nativeElement.style.backgroundColor = '#FFAD89';
+//     } else {
+//       this.elementRef.nativeElement.style.backgroundColor = '';
+//     }
+//   }
+// }
+import { Directive, ElementRef, Input, OnChanges, Host, Inject, Optional } from '@angular/core';
+import { TaskListComponent } from '../components/task-list/task-list.component';
 
 @Directive({
   selector: '[appBackgroundImportant]'
 })
 export class BackgroundImportantDirective implements OnChanges {
   @Input('appBackgroundImportant') important!: boolean;
+  @Input() dateEnd!: Date;
 
-  constructor(private elementRef: ElementRef) {}
+  constructor(
+    private elementRef: ElementRef,
+    @Host() @Optional() @Inject(TaskListComponent) private taskList: TaskListComponent
+  ) {}
 
   ngOnChanges() {
     if (this.important) {
-      this.elementRef.nativeElement.style.backgroundColor = '#FFAD89';
+      this.elementRef.nativeElement.style.backgroundColor = '#a17763';
     } else {
       this.elementRef.nativeElement.style.backgroundColor = '';
+    }
+
+    if (this.taskList) {
+      this.taskList.sortTasks();
     }
   }
 }
